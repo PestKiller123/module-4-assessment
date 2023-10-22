be599d5f7f849588c6adba00dfb458e91d650457
PestKiller123 <gwpixs@gmail.com>
2023-10-22 11:15:11 -0600

    fortune button working

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,11 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["A beautiful, smart, and loving person will be coming into your life.", "A fresh start will put you on your way.", "A faithful friend is a strong defense.", "A lifetime of happiness lies ahead of you.", "All your hard work will soon pay off."];
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+        res.status(200).send(randomFortune);
     }
-
 }
